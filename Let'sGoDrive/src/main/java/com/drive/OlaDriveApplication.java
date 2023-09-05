ea5bb6d153265e6e49f596d943e1c446d35683fd
Shaneen <jawalinikita06@gmail.com>
2023-09-05 14:42:42 +0530

    code updated

--- Let'sGoDrive/src/main/java/com/drive/OlaDriveApplication.java
+++ Let'sGoDrive/src/main/java/com/drive/OlaDriveApplication.java
@@ -2,7 +2,33 @@ package com.drive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Let's Go Drive App",
+				description = "Let's Go Drive App API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Nikita",
+						email = "nikitajawali0@gmail.com",
+						url = "https://www.nikita.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.nikita.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Let's Go Drive App Documentation",
+				url = "https://www.nikita.net/user_management.html"
+		)
+)
 public class OlaDriveApplication {
 
 	public static void main(String[] args) {
